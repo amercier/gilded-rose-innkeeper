078f7932cee6df5584bbdd695025426e03aef56b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-24 14:30:21 +0100

    Added Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 export default function App() {
   return (
@@ -22,5 +22,5 @@ export default function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
