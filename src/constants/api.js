d6032ee2d4d03b64eec6c93e637cbb9a907178b0
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-27 23:56:13 +0100

    Added support for custom API URL

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,14 @@
-// TODO Inject env.process.API_URL at build time
-export const API_URL_BASE = 'http://localhost:8080';
+/**
+ * Base API URL.
+ *
+ * Use `REACT_APP_API_URL` environment variable is defined, otherwise default to hardcoded value.
+ * Note: variable must start with `REACT_APP_`. Read [Adding Custom Environment Variables] for more
+ * information.
+ *
+ * [adding custom environment variables]: https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables
+ *
+ * @type {string}
+ */
+const API_URL_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
 export const API_URL_ITEMS = `${API_URL_BASE}/items/`;
