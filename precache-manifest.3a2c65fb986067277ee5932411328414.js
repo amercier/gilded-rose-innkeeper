e1a87916babde4e52b30d010c705d80b50ea7899
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-27 13:10:40 +0000

    Deploy amercier/gilded-rose-innkeeper to github.com/amercier/gilded-rose-innkeeper.git:gh-pages

---
+++ precache-manifest.3a2c65fb986067277ee5932411328414.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1e30831e1aeeaaa7740",
+    "url": "/static/js/main.c1e30831.chunk.js"
+  },
+  {
+    "revision": "ac5061da54c65d4799c1",
+    "url": "/static/js/1.ac5061da.chunk.js"
+  },
+  {
+    "revision": "d06a2e3aeb087e424b751a6c3d269f95",
+    "url": "/index.html"
+  }
+];
