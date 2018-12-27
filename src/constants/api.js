ec1cb119c02b8aa0eca21cbd1dc2678a5effda64
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-27 23:56:13 +0100

    Added support for custom API URL

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-// TODO Inject env.process.API_URL at build time
-export const API_URL_BASE = 'http://localhost:8080';
+export const API_URL_BASE =
+  process.env.REACT_APP_API_URL || 'http://localhost:8080';
 export const API_URL_ITEMS = `${API_URL_BASE}/items/`;
