5ed6ffb21d8605a2ebff75e1a88e6ff0e103aca4
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-31 08:43:38 +0100

    Added redux-logger in development

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
-import createDebounceDebounceMiddleware from 'redux-debounced';
+import createDebounceMiddleware from 'redux-debounced';
+import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import itemReducer from '../reducers/item';
 import { doFetchItems } from '../actions/item';
 import rootSaga from '../sagas';
 
-const debounce = createDebounceDebounceMiddleware();
+const debounce = createDebounceMiddleware();
 const saga = createSagaMiddleware();
 
 const store = createStore(
   itemReducer,
   undefined, // preloadedState - we don't need an initial state as we will run the root saga immediately
-  applyMiddleware(debounce, saga),
+  applyMiddleware(
+    ...(process.env.NODE_ENV === 'development'
+      ? [debounce, createLogger(), saga]
+      : [debounce, saga]),
+  ),
 );
 
-// Start listening for actions
+// Run the root saga (ie: start listening for actions)
 saga.run(rootSaga);
 
 // Trigger initial items fetch action
