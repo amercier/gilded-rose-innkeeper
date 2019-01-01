0dbf8c51d8c784dff0ac78667afd7e2f18192dfc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-01 09:29:00 +0000

    Deploy amercier/gilded-rose-innkeeper to github.com/amercier/gilded-rose-innkeeper.git:gh-pages

---
+++ precache-manifest.16d5bb769efb77b430fd5ac1d7734679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f82f5bf8566dc34c9b5",
+    "url": "/admin/static/js/runtime~main.3f82f5bf.js"
+  },
+  {
+    "revision": "21449d5f7d519e7a1c2a",
+    "url": "/admin/static/js/main.21449d5f.chunk.js"
+  },
+  {
+    "revision": "abeaae84e3b8575dfead",
+    "url": "/admin/static/js/1.abeaae84.chunk.js"
+  },
+  {
+    "revision": "21449d5f7d519e7a1c2a",
+    "url": "/admin/static/css/main.e6c20842.chunk.css"
+  },
+  {
+    "revision": "abeaae84e3b8575dfead",
+    "url": "/admin/static/css/1.f4f9c07a.chunk.css"
+  },
+  {
+    "revision": "e33ac1572166c5374fccb6ffab6b461f",
+    "url": "/admin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19f112b0dd450f67d2fa4a9b22917138.js"
+  "/admin/precache-manifest.16d5bb769efb77b430fd5ac1d7734679.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/admin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
