90aab0b0a64ae767f2769c4f6ccf816ea4d5698a
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-06 17:04:31 +0100

    Upgraded antd to latest version

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Provider store={store}>
